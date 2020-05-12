cf2a94f83f221ef15fa2dfbfc08d09c4672ba024
djnwzs <31347434+djnwzs@users.noreply.github.com>
2020-05-12 22:18:43 +0800

    Support preview

--- index.js
+++ index.js
@@ -56,6 +56,12 @@ const config = {
      * example: https://storage.idx0.workers.dev/Images/def.png?proxied
      */
     "proxyDownload": true,
+    /**
+     * File Preview
+     * example: https://storage.idx0.workers.dev/Video/def.mp4?preview
+     */
+    "proviewMode": true,
+    "previewsuffix": ['ogg', 'mp3', 'wav', 'm4a', 'mp4', 'webm', 'jpg', 'jpeg', 'png', 'gif', 'webp'],/*可预览的类型*/
 };
 
 /**
@@ -94,50 +100,50 @@ const USER_PASS_REGEXP = /^([^:]*):(.*)$/
  * Object to represent user credentials.
  */
 
-const Credentials = function(name, pass) {
-  this.name = name
-  this.pass = pass
+const Credentials = function (name, pass) {
+    this.name = name
+    this.pass = pass
 }
 
 /**
  * Parse basic auth to object.
  */
 
-const parseAuthHeader = function(string) {
-  if (typeof string !== 'string') {
-    return undefined
-  }
+const parseAuthHeader = function (string) {
+    if (typeof string !== 'string') {
+        return undefined
+    }
 
-  // parse header
-  const match = CREDENTIALS_REGEXP.exec(string)
+    // parse header
+    const match = CREDENTIALS_REGEXP.exec(string)
 
-  if (!match) {
-    return undefined
-  }
+    if (!match) {
+        return undefined
+    }
 
-  // decode user pass
-  const userPass = USER_PASS_REGEXP.exec(atob(match[1]))
+    // decode user pass
+    const userPass = USER_PASS_REGEXP.exec(atob(match[1]))
 
-  if (!userPass) {
-    return undefined
-  }
+    if (!userPass) {
+        return undefined
+    }
 
-  // return credentials object
-  return new Credentials(userPass[1], userPass[2])
+    // return credentials object
+    return new Credentials(userPass[1], userPass[2])
 }
 
 
-const unauthorizedResponse = function(body) {
-  return new Response(
-    null, {
-      status: 401,
-      statusText: "'Authentication required.'",
-      body: body,
-      headers: {
-        "WWW-Authenticate": 'Basic realm="User Visible Realm"'
-      }
-    }
-  )
+const unauthorizedResponse = function (body) {
+    return new Response(
+        null, {
+            status: 401,
+            statusText: "'Authentication required.'",
+            body: body,
+            headers: {
+                "WWW-Authenticate": 'Basic realm="User Visible Realm"'
+            }
+        }
+    )
 }
 
 async function handle(request) {
@@ -187,7 +193,7 @@ async function getAccessToken() {
         const data = await resp.json()
         _accessToken = data.access_token
         return _accessToken;
-    } else throw `getAccessToken error ${ JSON.stringify(await resp.text())}`
+    } else throw `getAccessToken error ${JSON.stringify(await resp.text())}`
 }
 
 
@@ -199,7 +205,7 @@ async function getAccessToken() {
  */
 function mime2icon(type) {
     if (type.startsWith("image")) return "image";
-    if (type.startsWith("image")) return "video_label";
+    if (type.startsWith("video")) return "video_label";
     if (type.startsWith("image")) return "audiotrack";
     return "description";
 }
@@ -291,7 +297,7 @@ async function handleFile(request, pathname, downloadUrl, {
 }
 
 async function handleUpload(request, pathname, filename) {
-    const url = `https://graph.microsoft.com/v1.0/me/drive/root:${config.base+(pathname.slice(-1) == "/" ? pathname :pathname + "/") }${filename}:/content`;
+    const url = `https://graph.microsoft.com/v1.0/me/drive/root:${config.base + (pathname.slice(-1) == "/" ? pathname : pathname + "/")}${filename}:/content`;
     return await fetch(url, {
         method: "PUT",
         headers: {
@@ -325,10 +331,10 @@ async function handleRequest(request) {
 
     const thumbnail = config.thumbnail ? searchParams.get("thumbnail") : false;
     const proxied = config.proxyDownload ? (searchParams.get("proxied") === null ? false : true) : false;
-
+    const preview = config.proviewMode ? (searchParams.get("preview") === null ? false : true) : false;
 
     if (thumbnail) {
-        const url = `https://graph.microsoft.com/v1.0/me/drive/root:${base+(pathname == "/" ? "" :pathname) }:/thumbnails/0/${thumbnail}/content`;
+        const url = `https://graph.microsoft.com/v1.0/me/drive/root:${base + (pathname == "/" ? "" : pathname)}:/thumbnails/0/${thumbnail}/content`;
         const resp = await fetch(url, {
             headers: {
                 "Authorization": `bearer ${accessToken}`
@@ -341,7 +347,8 @@ async function handleRequest(request) {
 
     }
 
-    const url = `https://graph.microsoft.com/v1.0/me/drive/root${ wrap_pathname(pathname) }?select=name,eTag,size,id,folder,file,%40microsoft.graph.downloadUrl&expand=children(select%3Dname,eTag,size,id,folder,file)`;
+
+    const url = `https://graph.microsoft.com/v1.0/me/drive/root${wrap_pathname(pathname)}?select=name,eTag,size,id,folder,file,%40microsoft.graph.downloadUrl&expand=children(select%3Dname,eTag,size,id,folder,file)`;
     const resp = await fetch(url, {
         headers: {
             "Authorization": `bearer ${accessToken}`
@@ -351,11 +358,15 @@ async function handleRequest(request) {
     if (resp.ok) {
         const data = await resp.json();
         if ("file" in data) {
-            return await handleFile(request, pathname, data["@microsoft.graph.downloadUrl"], {
-                proxied,
-                fileSize: data["size"]
-            });
-
+            if (preview) {/*预览模式*/
+                console.info('preview true');
+                return handlePreview(data["@microsoft.graph.downloadUrl"], suffix(data["name"]));/*渲染预览*/
+            }
+            else
+                return await handleFile(request, pathname, data["@microsoft.graph.downloadUrl"], {
+                    proxied,
+                    fileSize: data["size"]
+                });
         } else if ("folder" in data) {
             if (config.upload && request.method == "POST") {
                 const filename = searchParams.get("upload");
@@ -414,6 +425,8 @@ function renderFolderIndex(items, isIndex) {
     const el = (tag, attrs, content) => `<${tag} ${attrs.join(" ")}>${content}</${tag}>`;
     const div = (className, content) => el("div", [`class=${className}`], content);
     const item = (icon, filename, size) => el("a", [`href="${filename}"`, `class="item"`, size ? `size="${size}"` : ""], el("i", [`class="material-icons"`], icon) + filename)
+    const itemPV = (icon, filename, size) => el("a", [`href="${filename}?preview"`, `class="item"`, size ? `size="${size}"` : ""], el("i", [`class="material-icons"`], icon) + filename)
+
 
     return renderHTML(nav + div("container", div("items", el("div", ['style="min-width:600px"'],
         (!isIndex ? item("folder", "..") : "") +
@@ -421,7 +434,10 @@ function renderFolderIndex(items, isIndex) {
             if ("folder" in i) {
                 return item("folder", i.name, i.size)
             } else if ("file" in i) {
-                return item(mime2icon(i.file.mimeType), i.name, i.size)
+                if (inArray(suffix(i.name), config.previewsuffix)) {
+                    return itemPV(mime2icon(i.file.mimeType), i.name, i.size)
+                }
+                else return item(mime2icon(i.file.mimeType), i.name, i.size)
             } else console.log(`unknown item type ${i}`)
         }).join("")
     ))));
@@ -451,3 +467,147 @@ function renderHTML(body) {
     </body>
   </html>`
 }
+
+function inArray(val, arr) {
+    return arr.some(function (v) {
+        return val === v;
+    })
+}
+
+function handlePreview(url, suffix) {/*预览渲染器(文件直链，后缀)*/
+    if (inArray(suffix, config.previewsuffix)) {
+        template = previewHtml;
+        template = template.replace('{{url}}', url);
+        template = template.replace('{{suffix}}', suffix);
+        return new Response(template, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'content-type': 'text/html'
+            }
+        });
+    }
+    return new Response(null, {
+        status: 302,
+        headers: {
+            "Location": url.slice(6)
+        }
+    });
+}
+function suffix(filename) {/*从文件名后取出后缀名*/
+    var filename = filename;
+    //获取最后一个.的位置
+    var index = filename.lastIndexOf(".");
+    //获取后缀
+    var ext = filename.substr(index + 1);
+    ext = ext.toLowerCase();
+    return ext;
+}
+function fileGetContents(url) {
+    var fso, ts, s;
+    var ForReading = 1;
+    fso = new XMLHttpRequest("Scripting.FileSystemObject");
+    ts = fso.OpenTextFile(url, ForReading);
+    s = ts.ReadLine();
+    return s;
+}
+var previewHtml = `<html lang="cn">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge, chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Preview</title>
+    <link href="https://fonts.loli.net/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/gh/SomeBottle/OdIndex/assets/material.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.17.1/themes/prism-solarizedlight.css">
+	<style>
+	.vd{
+	   width:100%;
+	   display:none;
+	}
+	.pic{
+	   max-width:100%;
+	   max-height:600px;
+	   display:none;
+	}
+	.au{
+	   width:100%;
+	   display:none;
+	}
+	input{
+	   width:100%;
+	   border-top:none;
+	   border-left:none;
+	   border-right:none;
+	   margin:8px 0 8px 0;
+	}
+	label{
+	   font-size:8px;
+	   color:rgba(0,0,0,.54);
+	   pointer-events:none;
+	}
+	.ipt{
+	   margin:16px 0 8px 0;
+	}
+	.btn{
+	   background-color:#4b8e8d;
+	   color:white;
+	   border:none;
+	   cursor:hand;
+	   padding:10px;
+	   border-radius:5px;
+	}
+ 	</style>
+</head>
+<div id="night-mask" style="position: fixed;top: 0;left: 0;width: 100%;height: 100%;z-index: 2147483647;pointer-events: none;mix-blend-mode: multiply;transition: opacity 0.1s ease 0s;opacity:0.13;display:block;background: #000000;"></div>
+<body>
+    <nav><a class="brand">Od Preview</a></nav>
+    <div class="container">
+	    <audio id='au' class='au' src='' controls="controls"></audio>
+	    <img src='https://cdn.jsdelivr.net/gh/SomeBottle/OdIndex/assets/loading.png' class='pic' id='pic'></img>
+        <video class='vd' id='vd' controls="controls" src=""></video>
+		<div class='ipt'>
+		   <label>直链地址</label>
+		   <input type='text' id='dr'></input>
+		</div>
+		<button class='btn' onclick='down()'>直接下载</button>
+    </div>
+    <div style="flex-grow:1"></div>
+    <footer>
+        <p>Originally designed by <a href="https://github.com/heymind/OneDrive-Index-Cloudflare-Worker">Heymind</a>.</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/prismjs@1.17.1/prism.min.js" data-manual=""></script>
+    <script src="https://cdn.jsdelivr.net/npm/prismjs@1.17.1/plugins/autoloader/prism-autoloader.min.js"></script>
+    <style>
+        .othumb {
+            position: absolute;
+            background: #FAFAFA;
+            box-shadow: 0 0 5px #888;
+            border-radius: 8px;
+            transition: 1s ease;
+            overflow: hidden;
+            opacity: 0
+        }
+    </style>
+	<script>
+	var previewurl='{{url}}',previewtype='{{suffix}}',audios=['ogg','mp3','wav','m4a'],videos=['mp4','webm'],pics=['jpg','jpeg','png','gif','webp'];
+	if(audios.indexOf(previewtype)!==-1){
+	   document.getElementById('au').style.display='block';
+	   document.getElementById('au').src=previewurl;
+	}else if(videos.indexOf(previewtype)!==-1){
+	   document.getElementById('vd').style.display='block';
+	   document.getElementById('vd').src=previewurl;
+	}else if(pics.indexOf(previewtype)!==-1){
+	   document.getElementById('pic').style.display='block';
+	   document.getElementById('pic').src=previewurl;
+	}
+	var dl=location.href.split('?');
+	dl.pop();
+	dl=dl.join('?');
+	document.getElementById('dr').value=dl;
+	function down(){
+	   window.open(dl,'_self');
+	}
+	</script>
+</body>
+</html>
+`;
